dcf24f3a11bbb6adde857401183cea743d5bc2f8
AngelCortesM <joseangelcortesmontoya@gmail.com>
2024-04-07 15:54:44 -0500

    Añadir nuevas páginas y componentes, eliminar archivos CSS y optimizar estructura de directorios.

---
+++ src/Tabla.jsx
@@ -0,0 +1,18 @@
+export function Tabla() {
+    return (
+        <table className="table table-dark   table-striped table-hover" id="tabla">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Celular</th>
+                    <th>Direccion</th>
+                    <th>Correo Electronico</th>
+                </tr>
+            </thead>
+            <tbody id="bodytabla">
+
+            </tbody>
+        </table>
+    )
+}
